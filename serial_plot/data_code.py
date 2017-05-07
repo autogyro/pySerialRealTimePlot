f8d19118e57bed6a267bee9a5f661686901dd6b8
lisilin013 <lisilin013@163.com>
2017-05-07 16:55:06 +0800

    pyserial

---
+++ serial_plot/data_code.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 07 13:40:08 2017
+
+@author: lee
+"""
+
+import struct
+import ctypes
+import binascii
+import time
+import _const as const
+from str_convert  import * 
+
+
+# 接收报头报尾
+const.HEAD = 0xAA
+const.TAIL = 0x55
+const.LEN = 4*3         # 接收数据长度，需要修改
+
+# 接收状态机
+const.RX_FREE = 0
+const.RX_START = 1
+const.RX_DATA = 2
+
+'''
+  接收数据解码
+  input: ser.read()
+  output: list[float]
+'''
+def static_vars(**kwargs):
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+
+@static_vars(pit=0, rx_state=const.RX_FREE, buff=(ctypes.c_char*const.LEN)())
+def decode(raw_data):
+    data = binascii.b2a_hex(raw_data) # 转换为16进制字符
+    hex_data = int(data,16)           # 转换为int型
+
+    if(decode.rx_state==const.RX_FREE) :  # 报头
+        decode.pit = 0                    # 位置清零
+        if(hex_data==const.HEAD):
+            decode.rx_state = const.RX_START
+             
+    elif(decode.rx_state==const.RX_START):# 接收数据长度并校核
+        if(hex_data==const.LEN):
+            decode.rx_state = const.RX_DATA
+        else:
+            decode.rx_state = const.RX_FREE # 数据长度错误
+
+    elif(decode.rx_state==const.RX_DATA): # 接收数据
+        if(decode.pit<const.LEN):
+            decode.buff[decode.pit] = raw_data
+            decode.pit += 1
+        else:
+            decode.rx_state = const.RX_FREE     # 先复位在返回
+            if(hex_data==const.TAIL):           # 接收数据有效
+                return chars_buff_handler(decode.buff)  # 得到float数据
+                #print time.ctime()
+                #print chars_buff_handler(decode.buff)
+            
+
+'''
+  发送数据编码
+  input : list[float]
+  output: buff = (ctypes.c_char*(data_len*4+3))()
+'''
+def encode(float_data):
+    data_len = len(float_data)
+    pit = 2;
+    buff = (ctypes.c_char*(data_len*4+3))()
+
+    buff[0] = hex2cchar(str(const.HEAD).encode('ascii'))
+    #buff[1] = hex2cchar()
+    buff[data_len-1] = str(const.TAIL)
+
+    for f in float_data:
+        #bf = (ctypes.c_char*4)()
+        bf = float2cchar(f)
+        for i in range(0, len(bf)):
+            buff[pit] = bf[i]
+            pit += 1
+    return buff
+
---
+++ serial_plot/str_convert.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 07 13:41:11 2017
+
+@author: lee
+"""
+
+'''
+ 各种字符转换
+'''
+
+import struct
+import ctypes
+import binascii
+
+#class str_convert():
+
+'''
+    float和char之间转换
+'''
+class un_float_char (ctypes.Union):
+    _fields_ = [
+    ("fl",ctypes.c_float),  
+    ("ch",ctypes.c_char*4)
+    ]
+
+# 返回的是str类型，可用str[]访问
+def float_convert_char(_f):
+    un_data = un_float_char();
+    un_data.fl = ctypes.c_float(_f)
+    return un_data.ch
+
+def char_convert_float(_ch):
+    un_data = un_float_char();
+    un_data.ch = _ch
+    return un_data.fl
+
+# 接收数据str处理函数
+# input:  buff = (ctypes.c_char*const.LEN)()
+# output: list[float]
+def chars_buff_handler(_buff):
+    buff = []
+    for i in range(0,_buff._length_,4):
+        #buff.append(char_convert_float(_buff.raw[i:i+4])) # 会有数据接收不对
+        buff.append(cchar2float(_buff.raw[i:i+4]))
+    return buff
+
+'''
+    float和hex之间转换
+'''
+def cchar2hex(s): # bytes to hex
+    return binascii.b2a_hex(s)
+
+def hex2float(h):
+    return struct.unpack('f', h.decode('hex'))[0]
+
+def cchar2float(s):  # 串口数据解码成 float
+    return hex2float(cchar2hex(s))
+
+
+def float2hex(f):
+    return struct.pack("<f", f).encode('hex')
+
+def hex2cchar(h): # hex to bytes
+    return binascii.a2b_hex(h)
+
+def float2cchar(f): # float 编码成串口发送
+    return hex2cchar(float2hex(f))
+
+#import sys
+#sys.modules[__name__] = str_convert()
+
