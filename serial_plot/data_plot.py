fdb38aec2c90dc820e7a2561e7c2bef893c6a73e
lisilin013 <lisilin013@163.com>
2017-05-08 13:12:32 +0800

    data plot ok

--- serial_plot/data_plot.py
+++ serial_plot/data_plot.py
@@ -1,57 +1,96 @@
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-
-x = []
-y = []
-
-
-fig = plt.figure()
-ax1 = fig.add_subplot(2, 1, 1, xlim=(0, 2), ylim=(-4, 4))
-ax2 = fig.add_subplot(2, 1, 2, xlim=(0, 2), ylim=(-4, 4))
-line, = ax1.plot([], [], lw=2)
-line2, = ax2.plot([], [], lw=2)
+import wx
+from matplotlib.figure import Figure
+import matplotlib.font_manager as font_manager
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+# wxWidgets object ID for the timer
 
 
-def init():
+# number of data points
+POINTS = 300
+y_lim = 1000
+y_tick = 100
 
 
+class PlotFigure(wx.Frame):
+    """Matplotlib wxFrame with animation effect"""
 
-def update(buffer):
+    def __init__(self, plot_title, update_data):
+        wx.Frame.__init__(self, None, wx.ID_ANY,
+                          title=plot_title, size=(600, 400))
+        self.data = update_data
+        # Matplotlib Figure
+        self.fig = Figure((6, 4), 100)
+        # bind the Figure to the backend specific canvas
+        self.canvas = FigureCanvas(self, wx.ID_ANY, self.fig)
+        # add a subplot
+        self.ax = self.fig.add_subplot(111)
+        # limit the X and Y axes dimensions
+        self.ax.set_ylim([-y_lim, y_lim])
+        self.ax.set_xlim([0, POINTS])
 
-    return line,
+        self.ax.set_autoscale_on(False)
+        self.ax.set_xticks([])
+        # we want a tick every 10 point on Y (101 is to have 10
+        self.ax.set_yticks(range(-y_lim, y_lim + 1, y_tick))
+        # disable autoscale, since we don't want the Axes to ad
+        # draw a grid (it will be only for Y)
+        self.ax.grid(True)
+        # generates first "empty" plots
+        self.user = [None] * POINTS
+        # self.l_user, = self.ax.plot(range(POINTS), self.user, label='Acc_X')
+        self.l_user, = self.ax.plot(
+            range(POINTS), self.user, label='baby i love you!')
 
-fig1 = plt.figure()
+        # add the legend
+        self.ax.legend(loc='upper center',
+                           ncol=4,
+                           prop=font_manager.FontProperties(size=10))
+        # force a draw on the canvas()
+        # trick to show the grid and the legend
+        self.canvas.draw()
+        # save the clean background - everything but the line
+        # is drawn and saved in the pixel buffer background
+        self.bg = self.canvas.copy_from_bbox(self.ax.bbox)
+        # bind events coming from timer with id = TIMER_ID
+        # to the onTimer callback function
+        self.TIMER_ID = wx.NewId()
+        wx.EVT_TIMER(self, self.TIMER_ID, self.onTimer)
+        self.timer = wx.Timer(self, self.TIMER_ID)
 
-data = np.random.rand(2, 100)
-l, = plt.plot([], [], 'r-')
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-plt.xlabel('x')
-plt.title('test')
-line_ani = animation.FuncAnimation(fig1, update_line, 100, fargs=(data, l),
-                                   interval=50, blit=True)
+    def onTimer(self, evt):
+        """callback function for timer events"""
+        # restore the clean background, saved at the beginning
+        self.canvas.restore_region(self.bg)
+        # update the data
+        # temp = np.random.randint(10, 80)
+        temp = self.data[0]
+        self.user = self.user[1:] + [temp]
+        # update the plot
+        self.l_user.set_ydata(self.user)
+        # just draw the "animated" objects
+        # It is used to efficiently update Axes data (axis ticks, labels, etc
+        # are not updated)
+        self.ax.draw_artist(self.l_user)
+        self.canvas.blit(self.ax.bbox)
 
-# To save the animation, use the command: line_ani.save('lines.mp4')
+        ymin, ymax = self.ax.get_ylim()  # update xlim and ylim
+        if (temp >= ymax):
+            self.ax.set_ylim(ymin + temp - ymax + 200,
+                             temp + 200)
 
-fig2 = plt.figure()
+            # ymin, ymax = self.ax.get_ylim()
+            self.canvas.draw()
+            # self.ax.set_yticks(range(ymin, ymax+1, y_tick))
+        elif (temp <= ymin):
+            self.ax.set_ylim(temp - 200,
+                             ymax + temp - ymin - 200)
+            self.canvas.draw()
 
-x = np.arange(-9, 10)
-y = np.arange(-9, 10).reshape(-1, 1)
-base = np.hypot(x, y)
-ims = []
-for add in np.arange(15):
-    ims.append((plt.pcolor(x, y, base + add, norm=plt.Normalize(0, 30)),))
+    def __del__(self):
+        self.timer.Stop()
 
-im_ani = animation.ArtistAnimation(fig2, ims, interval=50, repeat_delay=3000,
-                                   blit=True)
-# To save this second animation with some metadata, use the following command:
-# im_ani.save('im.mp4', metadata={'artist':'Guido'})
-
-fig = plt.figure()
-
-
-
-plt.show()
+    def start(self):
+        self.timer.Start(50)
+        self.Show()
--- serial_plot/main_serial_read_plot.py
+++ serial_plot/main_serial_read_plot.py
@@ -17,6 +17,7 @@ import threading
 import time
 
 import serial
+import wx
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -24,80 +25,14 @@ import matplotlib.animation as animation
 import _const as const
 from str_convert import *
 from data_code import *
-
-import wx
-from matplotlib.figure import Figure
-import matplotlib.font_manager as font_manager
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-# wxWidgets object ID for the timer
-TIMER_ID = wx.NewId()
-# number of data points
-POINTS = 300
-global data
-y_lim = 1000
+from data_plot import PlotFigure
 
 
-class PlotFigure(wx.Frame):
-    """Matplotlib wxFrame with animation effect"""
-    global data
+global pitch
+global roll
+pitch = [0]
+roll = [0]
 
-    def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY,
-                          title="Serial Data Plot", size=(600, 400))
-        # Matplotlib Figure
-        self.fig = Figure((6, 4), 100)
-        # bind the Figure to the backend specific canvas
-        self.canvas = FigureCanvas(self, wx.ID_ANY, self.fig)
-        # add a subplot
-        self.ax = self.fig.add_subplot(111)
-        # limit the X and Y axes dimensions
-        self.ax.set_ylim([0, 100])
-        self.ax.set_xlim([0, POINTS])
-
-        self.ax.set_autoscale_on(False)
-        self.ax.set_xticks([])
-        # we want a tick every 10 point on Y (101 is to have 10
-        self.ax.set_yticks(range(0, 101, 10))
-        # disable autoscale, since we don't want the Axes to ad
-        # draw a grid (it will be only for Y)
-        self.ax.grid(True)
-        # generates first "empty" plots
-        self.user = [None] * POINTS
-        self.l_user, = self.ax.plot(range(POINTS), self.user, label='Acc_X')
-
-        # add the legend
-        self.ax.legend(loc='upper center',
-                           ncol=4,
-                           prop=font_manager.FontProperties(size=10))
-        # force a draw on the canvas()
-        # trick to show the grid and the legend
-        self.canvas.draw()
-        # save the clean background - everything but the line
-        # is drawn and saved in the pixel buffer background
-        self.bg = self.canvas.copy_from_bbox(self.ax.bbox)
-        # bind events coming from timer with id = TIMER_ID
-        # to the onTimer callback function
-        wx.EVT_TIMER(self, TIMER_ID, self.onTimer)
-
-    def onTimer(self, evt):
-        """callback function for timer events"""
-        # restore the clean background, saved at the beginning
-        self.canvas.restore_region(self.bg)
-        # update the data
-        # temp = np.random.randint(10, 80)
-        temp = data
-        self.user = self.user[1:] + [temp]
-        # update the plot
-        self.l_user.set_ydata(self.user)
-        # just draw the "animated" objects
-        # It is used to efficiently update Axes data (axis ticks, labels, etc
-        # are not updated)
-        self.ax.draw_artist(self.l_user)
-        self.canvas.blit(self.ax.bbox)
-
-
-    # def __del__(self):
-    #     t.Stop()
 
 
 '''
@@ -108,7 +43,8 @@ class PlotFigure(wx.Frame):
 
 
 def serial_thread():
-    global data
+    global pitch
+    global roll
 
     ser = serial.Serial('/dev/ttyUSB0', baudrate=38400,
                         timeout=1)  # open first serial port
@@ -124,7 +60,9 @@ def serial_thread():
                 print time.ctime()
                 print receive_buff
 
-                data = receive_buff[0]
+                pitch[0] = receive_buff[0]
+                roll[0] = receive_buff[1]
+
         time.sleep(0.001)
     ser.close()             # close port
 
@@ -141,10 +79,17 @@ if(__name__ == '__main__'):
         th.start()
 
     app = wx.PySimpleApp()
-    frame = PlotFigure()
-    t = wx.Timer(frame, TIMER_ID)
-    t.Start(50)
-    frame.Show()
+
+    # class example
+    global pitch
+    global roll
+
+    frame = PlotFigure('acc x', pitch)
+    frame.start()
+
+    frame_ = PlotFigure('acc y', roll)
+    frame_.start()
+
     app.MainLoop()
 
     th.join()
